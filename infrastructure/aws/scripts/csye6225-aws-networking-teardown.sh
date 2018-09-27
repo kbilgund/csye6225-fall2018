9b14faf1d535ca8a8c8ae7811ebe8f60840e0e18
rohanmandhanya <36527473+rohanmandhanya@users.noreply.github.com>
2018-09-26 18:18:04 -0700

    teardown script

--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -6,20 +6,17 @@ subnet1="{STACK_NAME}-csye6225-subnet1"
 subnet2="{STACK_NAME}-csye6225-subnet2"
 subnet3="{STACK_NAME}-csye6225-subnet3"
 
-
-
-ig_id=$(aws ec2 describe-internet-gateway | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value==internetgateway_name)| .InternetGatewayId' -r)
-vpc_id=$(aws ec2 describe-vpc | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value==vpc_name)| .VpcId' -r)
-rt_id=$(aws ec2 describe-route-tables | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value==routetable_name)| .RouteTableId' -r)
-# sub1=$(aws ec2 describe-subnets | jq '.Subnets[1].SubnetId' -r)
-# sub2=$(aws ec2 describe-subnets | jq '.Subnets[3].SubnetId' -r)
-# sub3=$(aws ec2 describe-subnets | jq '.Subnets[2].SubnetId' -r)
- # echo ${rt_id}
-
+ig_id=$(aws ec2 describe-internet-gateways | jq '.InternetGateways | .[] | select(.Tags[0] != null) | select(.Tags[0].Value=="internetgateway_name")| .InternetGatewayId' -r)
+vpc_id=$(aws ec2 describe-route-tables | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value=="routetable_name")| .VpcId' -r)
+rt_id=$(aws ec2 describe-route-tables | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value=="routetable_name")| .RouteTableId' -r)
+sub1=$(aws ec2 describe-route-tables | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value=="routetable_name")| .Associations | .[0] | .SubnetId' -r)
+sub2=$(aws ec2 describe-route-tables | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value=="routetable_name")| .Associations | .[1] | .SubnetId' -r)
+sub3=$(aws ec2 describe-route-tables | jq '.RouteTables | .[] | select(.Tags[0] != null) | select(.Tags[0].Value=="routetable_name")| .Associations | .[2] | .SubnetId' -r)
+ 
 aws ec2 detach-internet-gateway --internet-gateway-id $ig_id --vpc-id $vpc_id
 aws ec2 delete-internet-gateway --internet-gateway-id $ig_id
-# aws ec2 delete-subnet --subnet-id $sub1
-# aws ec2 delete-subnet --subnet-id $sub2
-# aws ec2 delete-subnet --subnet-id $sub3
+aws ec2 delete-subnet --subnet-id $sub1
+aws ec2 delete-subnet --subnet-id $sub2
+aws ec2 delete-subnet --subnet-id $sub3
 aws ec2 delete-route-table --route-table-id $rt_id
 aws ec2 delete-vpc --vpc-id $vpc_id
