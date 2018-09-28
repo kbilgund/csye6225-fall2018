cb6a352654f60c48ab02626442a67010777d4f4b
Kiran Bilgundi <kiranbilgundi1@gmail.com>
2018-09-28 11:39:53 -0700

    Web app initial commit
    This folder has basic web application which implements basic auth for rest end points

---
+++ webapp/cloudClass1/src/main/java/com/demo/security/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.demo.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private BasicAuthEntryPoint basicAuthEntryPoint;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(basicAuthEntryPoint)
+                ;
+
+    }
+
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select name as username,password, 1 as enabled from user where name=?")
+                .authoritiesByUsernameQuery("select name as username, 'ADMIN' as role from user where name=?");
+    }
+
+
+
+
+}
