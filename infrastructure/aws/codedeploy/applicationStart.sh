2fe5ea16772755d674ddc95540bf111895a00f27
Kiran Bilgundi <kiranbilgundi1@gmail.com>
2018-10-24 15:17:16 -0700

    Update applicationStart.sh

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/centos/
 sudo chown -R centos:centos /home/centos/cloudClass1-1.0-SNAPSHOT.jar
-sudo nohup java -jar -Dspring.profiles.active=prod cloudClass1-1.0-SNAPSHOT.jar > /dev/null 2>&1  &
+sudo nohup java -jar -Dspring.profiles.active=dev cloudClass1-1.0-SNAPSHOT.jar > /dev/null 2>&1  &
