786dfd37a35963630e5958187125c850ae92739d
Rohan Mandhanya <36527473+rohanmandhanya@users.noreply.github.com>
2018-10-25 18:45:40 -0700

    Updated Scripts

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -1,6 +1,9 @@
 echo "stack name $1"
+Domain="code-deploy.csye6225-fall2018-${2}"
+cdarn="arn:aws:s3:::code-deploy.csye6225-fall2018-${2}/*"
+csarn="arn:aws:s3:::csye6225-fall2018-${2}.csye6225.com/*"
 
-aws cloudformation create-stack --stack-name ${1} --template-body file://csye6225-cf-cicd.json --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name ${1} --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=DomainName,ParameterValue=$Domain ParameterKey=CDARN,ParameterValue=$cdarn ParameterKey=CSARN,ParameterValue=$csarn --capabilities CAPABILITY_NAMED_IAM
 echo "creating"
 aws cloudformation wait stack-create-complete --stack-name ${1}
 echo "created"
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
@@ -3,6 +3,7 @@ echo "stack name $1"
 aws s3 rm s3://csye6225-fall2018-${2}.csye6225.com --recursive
 aws s3 rm s3://code-deploy.csye6225-fall2018-${2} --recursive
 
+
 aws cloudformation delete-stack --stack-name ${1}
 echo "deleting"
 aws cloudformation wait stack-delete-complete --stack-name ${1}
