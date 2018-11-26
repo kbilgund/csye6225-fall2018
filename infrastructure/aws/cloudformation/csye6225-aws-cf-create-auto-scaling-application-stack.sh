94739a24a04e3068f938e7db16ed4566bfa2fe13
Rohan Mandhanya <36527473+rohanmandhanya@users.noreply.github.com>
2018-11-25 16:17:35 -0800

    Updated stacks

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -4,11 +4,15 @@ echo Domain-${3}
 VpcName="${2}-csye6225-vpc"
 IgName="${2}-csye6225-InternetGateway"
 Domain="csye6225-fall2018-${3}.csye6225.com"
+DNS="csye6225-fall2018-${3}.me."
 
 echo $VpcName
 echo $RouteTableName
 echo $Domain
 
+
+CDArn=$(aws iam get-role --role-name CodeDeployServiceRole | jq .Role.Arn -r)
+echo "CDArn" $CDArn
 VpcId=$(aws ec2 describe-vpcs --query  'Vpcs[?Tags[?Key==`Name`]|[?Value==`'${VpcName}'`]].VpcId' --output text)
 echo "VpcId" $VpcId
 ELB3=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=PublicSubnet | jq '.Subnets[0].SubnetId' -r)
@@ -21,7 +25,7 @@ PSubnetId=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=PrivateSubne
 GSubnetId=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=PrivateSubnet2 | jq '.Subnets[0].SubnetId' -r)
 echo "GroupSubnetId" $PSubnetId $GSubnetId
 CertificateArn=$(aws acm list-certificates | jq .CertificateSummaryList[0].CertificateArn -r)
-aws cloudformation create-stack --stack-name ${1} --template-body file://csye6225-cf-auto-scaling-application.json  --parameters ParameterKey=VpcId,ParameterValue=$VpcId ParameterKey=ELBId1,ParameterValue=$ELB1 ParameterKey=ELBId2,ParameterValue=$ELB2 ParameterKey=ELBId3,ParameterValue=$ELB3 ParameterKey=GroupSubnetId1,ParameterValue=$PSubnetId ParameterKey=GroupSubnetId2,ParameterValue=$GSubnetId ParameterKey=DomainName,ParameterValue=$Domain ParameterKey=CertificateArn,ParameterValue=$CertificateArn
+aws cloudformation create-stack --stack-name ${1} --template-body file://csye6225-cf-auto-scaling-application.json  --parameters ParameterKey=VpcId,ParameterValue=$VpcId ParameterKey=ELBId1,ParameterValue=$ELB1 ParameterKey=ELBId2,ParameterValue=$ELB2 ParameterKey=ELBId3,ParameterValue=$ELB3 ParameterKey=GroupSubnetId1,ParameterValue=$PSubnetId ParameterKey=GroupSubnetId2,ParameterValue=$GSubnetId ParameterKey=DomainName,ParameterValue=$Domain ParameterKey=Domain,ParameterValue=$DNS ParameterKey=CertificateArn,ParameterValue=$CertificateArn ParameterKey=CDArn,ParameterValue=$CDArn
 echo "creating"
 aws cloudformation wait stack-create-complete --stack-name ${1}
 echo "created"
