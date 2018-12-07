0cda51ae55149d16e0e083c486cb16349e8f10a9
Kiran Bilgundi <kiranbilgundi1@gmail.com>
2018-12-07 11:03:08 -0800

    Update applicationStop.sh

--- infrastructure/aws/codedeploy/applicationStop.sh
+++ infrastructure/aws/codedeploy/applicationStop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-process_id=`/bin/ps -ef | grep "nohup" | grep -v "grep" | awk '{print $2}'`
+process_id=`/bin/ps -ef | grep "java -jar" | grep -v centos | awk '{print $2}'`
 echo $process_id
 sudo kill -9 $process_id
 exit 0
